4272e58e53082629c4be9946fe1b863234607697
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-04-12 21:06:43 -0400

    commit tree 2c724520695483c34650403f79a35d7e3b4dcfb4

--- main.js
+++ main.js
@@ -2,7 +2,37 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+//grab all heart elements from HTML
+const hearts = document.querySelectorAll(".like")
+
+//iterate through each heart and add a click event listener to each
+hearts.forEach(heart => {
+  heart.addEventListener('click', (e) => {
+    //for each heart, mimic a server request that returns either a success or failure status
+    mimicServerCall()
+    //if server returns a success status, use if statement to change like heart
+    //from empty to full/red and vis versa
+    .then(sucess => {
+      if (e.target.textContent === EMPTY_HEART) {
+        e.target.textContent = FULL_HEART
+        e.target.className = 'activated-heart'
+      } else if (e.target.textContent === FULL_HEART) {
+        e.target.textContent = EMPTY_HEART
+        e.target.className = ''
+      }
+    })
+    //if server returns a failure status, display error model by changing class name to an empty string
+    //display error in error modal
+    .catch(error => {
+      document.querySelector('#modal').className = ''
+      document.querySelector('#modal-message').textContent = error
+      //error displays for 3 seconds then disappears
+      setTimeout(() => {document.querySelector('#modal').className = 'hidden'}, 3000)
+    })
+  })
+})
+
+
 
 
 
